dd885aa385b765fdac4735f8024d83bda1027100
nakulchawla09 <nakulchawla09@gmail.com>
2018-04-23 14:24:49 -0700

    Add xmodmap

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc bashrc bash_profile zpreztorc zshrc tmux.conf gitconfig bash_aliases"        # list of files/folders to symlink in homedir
+files="vimrc bashrc bash_profile zpreztorc zshrc tmux.conf gitconfig bash_aliases xmodmap"        # list of files/folders to symlink in homedir
 
 ##########
 

e1eb671e054cb31ebad6a7387eaaf82976494137
thenakulchawla <msg@nakul.net>
2025-05-31 16:48:27 -0700

    update with correct path

--- copyfiles.sh
+++ copyfiles.sh
@@ -4,9 +4,9 @@
 ############################
 
 files="vimrc xvimrc bashrc bash_profile zpreztorc zshrc tmux.conf gitconfig
-bash_aliases xmodmap ideavimrc"
+bash_aliases"
 
 for file in $files; do
     echo "Copying file to home"
-    yes | cp ~/$file ~/.$file
+    yes | cp ~/dotfiles/$file ~/.$file
 done
